c2b90f85f4b412b8009f2577db03527dddb767ca
Thanh Hai <haiduong.010102@gmail.com>
2023-10-04 10:57:27 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+/*
+ * Comment trong Javascript
+ * 1. Mục đích sử dụng 
+ * - Ghi chú
+ * - Vô hiệu hóa mã
+ * 
+ * 2. Loại comments
+ * - Comment 1 dòng
+ * - Comment nhiều dòng
+ * 
+ * 3. Sử dụng phím tắt
+ * - Ctrl + /
+ */
+
+// khai báo biến 
+var fullName = 'Duong Thanh Hai';
+var age = 21; // khai báo tuổi
+
+
+// Gọi hàm trong thông báo
+alert(fullName);
+alert(age);
+
+console.log(fullName)
+
+confirm('Xac nhan ban du tuoi!');
+
+prompt('Xac nhan ban du tuoi!');
+
+setTimeout(function() {
+    alert('thong bao')
+}, 1000)
+
+//setInterval(function(){
+  //  console.log('Thong bao' + Math.random())
+// }, 1000)
